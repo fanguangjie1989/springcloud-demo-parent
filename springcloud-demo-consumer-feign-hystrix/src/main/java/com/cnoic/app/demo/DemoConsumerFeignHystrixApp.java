1b11ed1d8cb42267db36bd3814e51994f4dd9aaf
fanguangjie1989 <fanguangjie1989@163.com>
2018-06-19 12:15:06 +0800

    fix hystrix bug

--- springcloud-demo-consumer-feign-hystrix/src/main/java/com/cnoic/app/demo/DemoConsumerFeignHystrixApp.java
+++ springcloud-demo-consumer-feign-hystrix/src/main/java/com/cnoic/app/demo/DemoConsumerFeignHystrixApp.java
@@ -3,9 +3,11 @@ package com.cnoic.app.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
 public class DemoConsumerFeignHystrixApp {
